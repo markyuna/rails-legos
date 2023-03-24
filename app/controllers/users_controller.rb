f49174b1661345d14b7dcbf84974a2685c7fdc3e
Emmanuel Dedryver <marcossuarezr88@gmail.com>
2023-03-24 16:06:24 +0100

    update avatar menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
-  # before_action :authenticate_user!
-  # before_action :set_user, only: %i[show]
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[show]
 
-  # def show
-  #   @products = current_user.products.all
-  #   @product = current_user.reviews.all
-  # end
+  def show
+    @products = current_user.products.all
+    @product = current_user.reviews.all
+  end
 
-  # private
+  private
 
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
