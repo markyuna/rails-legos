31782720fbdafc2b2b2b2fc7315e9774942de7b8
Emmanuel Dedryver <marcossuarezr88@gmail.com>
2023-04-02 00:56:58 +0200

    mapbox updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,7 @@ class ProductsController < ApplicationController
       {
         lat: product.latitude,
         lng: product.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { product: product }),
+        info_window_html: render_to_string(partial: "info_window", locals: { product: product }),
         marker_html: render_to_string(partial: "marker", locals: {product: product})
       }
     end
